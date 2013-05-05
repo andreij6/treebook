a55162a8c69029ea11d578797a5bc90dbce55b79
Andre I. Jones <andreij6@gmail.com>
2013-05-04 19:17:54 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GIS::Application.config.secret_token = '1b3971aa1857e0d3506523332ad39a27a04cbc6ae351533de110113c0a1d3fdcab6de7b0817acbf81b716f03e24f7214d80a3bcd7659ef886dc61dcc50bb1a93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GIS::Application.config.session_store :cookie_store, key: '_GIS_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GIS::Application.config.session_store :active_record_store
